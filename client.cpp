41c46f610db2c410ecb7200d3184f84f1033510c
gxher <2650598513@qq.com>
2022-05-28 22:43:24 +0800

    cn_design

---
+++ client.cpp
@@ -0,0 +1,281 @@
+﻿#pragma warning(disable:4996)  
+#include "Winsock.h"
+#include "windows.h"
+#include "stdio.h"
+#include "time.h"
+#include <iostream>
+#include<stdlib.h>
+#include "client.h"
+
+using namespace std;
+#define RECV_PORT 4523
+#define SEND_PORT 4623  
+#pragma comment(lib, "wsock32.lib")
+SOCKET sockclient;
+char filename[20];                       //文件名  
+sockaddr_in ServerAddr;                 //服务器地址
+char rbuff[1024];                        //接收缓冲区  
+char sbuff[1024];                       //发送缓冲区  
+char InputIP[20];                       //存储输入的服务器IP
+
+void welcome()
+{
+    cout << "                                                                                              " << endl
+        << "                                                                                              " << endl
+        << "                                                                                              " << endl
+        << "                                                                                              " << endl
+        << "          ##########    ###        ###     ###   ##########j       ##########  ############, D#########." << endl
+        << "          ##########    ####       ###    ###,   ##########j       ##########  ############, D##########." << endl
+        << "          ##D          i####       ###   i###    ###               ###              ##       D##      ###" << endl
+        << "          ##D          ##W##;      ###   ###     ###               ###              ##       D##      ###" << endl
+        << "          ##D          ## ###      ###  ###      ###               ###              ##       D##      D##" << endl
+        << "          ##D         K##  ##      ### ###       ###               ###              ##       D##      ###" << endl
+        << "          ##D         ##t  ###     ######L       #########         ###              ##       D##      ###" << endl
+        << "          #########  ;##   ###     #######       #########         #########j       ##       D##########E" << endl
+        << "          #########  ###   i##,    #######,      ###jjjjjj         #########j       ##       D#########E" << endl
+        << "          ##D        ###    ###    #### ###      ###               ###              ##       D#######G" << endl
+        << "          ##D       K##########    ###  .###     ###               ###              ##       D##" << endl
+        << "          ##D       ###########    ###   W##D    ###               ###              ##       D##" << endl
+        << "          ##D       ##D      ###   ###    ###    ###               ###              ##       D##" << endl
+        << "          ##D      E##       W##   ###     ###   ###jjjjjjji       ###              ##       D##" << endl
+        << "          ##D      ###        ##D  ###     W##W  ##########D       ###              ##       D##" << endl
+        << "          ##D      ##t        ###  ###      ###  ##########D       ###              ##       D##" << endl
+        << endl << endl;
+    system("pause");
+    system("cls");
+}
+
+void help()//帮助菜单
+{
+    cout << "                      weolcome to the menu of FAKE FTP              " << endl
+        << "                 _____________________________________________       " << endl
+        << "                  1.get:                                          " << endl
+        << "                              function: download files                       " << endl
+        << "                              usage   : get filename                         " << endl << endl
+        << "                  2.put:                                          " << endl
+        << "                              function: upload files                       " << endl
+        << "                              usage   : put filename                         " << endl << endl
+        << "                  3.pwd:                                          " << endl
+        << "                              function: Displays the current absolute path" << endl
+        << "                              usage   : pwd                        " << endl << endl
+        << "                  4.ls:                                           " << endl
+        << "                              function: Displays all files in the current directory" << endl
+        << "                              usage   : ls                        " << endl << endl
+        << "                  5.cd:       " << endl
+        << "                              function: switch working Directory             " << endl
+        << "                              usage: cd pathname             " << endl
+        << "                  6.help       " << endl
+        << "                  7.quit       " << endl
+        << "                  _____________________________________________       " << endl;
+}
+void list(SOCKET sockfd)
+{
+    int nRead;
+    while (true)
+    {
+        nRead = recv(sockclient, rbuff, 1024, 0); //接收服务端返回的
+             
+        if (nRead == SOCKET_ERROR)
+        {
+            printf("read response error!\n");
+            exit(1);
+        }
+        if (nRead == 0)//数据读取结束        
+            break;
+        //显示数据   
+        rbuff[nRead] = '\0';
+        printf("%s", rbuff);
+    }
+}
+
+int SendFile(SOCKET datatcps, FILE* file)
+{
+    printf(" sending file data..");
+    for (;;)  //从文件中循环读取数据并发送客户端       
+    {
+        int r = fread(sbuff, 1, 1024, file);//fread函数从file文件读取1个1024长度的数据到sbuff，返回成功读取的元素个数            
+        if (send(datatcps, sbuff, r, 0) == SOCKET_ERROR)
+        {
+            printf("lost the connection to client!\n");
+            closesocket(datatcps);
+            return 0;
+        }
+        if (r < 1024)                      //文件传送结束    
+            break;
+    }
+    closesocket(datatcps);
+    printf("done\n");
+    return 1;
+}
+
+DWORD StartSock()//启动winsock
+{
+    WSADATA WSAData;
+    char buf[20];
+    memset(buf, 0, 20);// 清空buf
+    WORD wVersionRequested;
+    wVersionRequested = MAKEWORD(2, 2); //设置版本号
+    if (WSAStartup(wVersionRequested, &WSAData) != 0)//加载winsock版本
+    {
+        printf("sock init fail!\n");
+        return (-1);
+    }
+    if (strncmp(InputIP, buf, 20) == 0)
+    {
+        printf("Please enter the IP address of the connected host：");
+        scanf("%s", &InputIP);
+    }
+    //设置地址结构
+    ServerAddr.sin_family = AF_INET;//指定IP地址版本IPV4
+    ServerAddr.sin_addr.s_addr = inet_addr(InputIP);//指定服务器IP
+    ServerAddr.sin_port = htons(RECV_PORT);//设置端口号
+    return(1);
+}
+  
+DWORD CreateSocket()
+{
+    sockclient = socket(AF_INET, SOCK_STREAM, 0);//当socket函数成功调用时返回一个新的SOCKET(Socket Descriptor)
+    if (sockclient == SOCKET_ERROR)
+    {
+        printf("sockclient create fail! \n");
+        WSACleanup();
+        return(-1);
+    }
+    return(1);
+}
+DWORD CallServer() //发送连接请求  
+{
+    CreateSocket();
+
+    if (connect(sockclient, (struct  sockaddr*)&ServerAddr, sizeof(ServerAddr)) == SOCKET_ERROR)
+    {
+        printf("Connect fail \n");
+        memset(InputIP, 0, 20); //连接失败则清空之前的输入
+        return(-1);
+    }
+    return(1);
+}
+
+DWORD TCPSend(char data[])   //发送命令  
+{
+    int length;
+    length = send(sockclient, data, strlen(data), 0);
+    //send函数通过sockclient接口发送data里面的数据，发送成功返回发送的字节数  
+    if (length <= 0)
+    {
+        printf("send data error ! \n");
+        closesocket(sockclient);
+        WSACleanup();
+        return(-1);
+    }
+    return(1);
+}
+
+int main()
+{
+    welcome();
+    while (1)
+    {
+        char messge1[10];           //定义输入要处理的文件名  
+        char messge2[20];           //定义输入要处理的文件名
+        char order[30];             //输入的命令    
+        order[0] = '\0';
+        char buff[80];              //用以存储经过字串格式化的order  
+        FILE* fd;                   //File协议主要用于访问本地计算机中的文件，fd指针指向要访问的目标文件  
+        FILE* fd2;
+        int count;
+        int sin_size = sizeof(ServerAddr);
+        StartSock();
+        if (CallServer() == -1)
+            return main();          //发送连接请求失败，返回主函数
+
+        printf("\nEnter your order（enther 'help' Enter the menu）:\n");
+        memset(buff, 0, 80);            //清空数组   
+        memset(messge2, 0, 20);
+        memset(order, 0, 30);
+        memset(messge1, 0, 10);
+        memset(rbuff, 0, 1024);
+        memset(sbuff, 0, 1024);
+
+        scanf("%s", &messge1);//s%输入字符串
+        if (strncmp(messge1, "get", 3) == 0)
+            scanf("%s", &messge2);
+        if (strncmp(messge1, "put", 3) == 0)
+            scanf("%s", &messge2);
+        if (strncmp(messge1, "cd", 2) == 0)
+            scanf("%s", &messge2);
+        strcat(order, messge1);         //把messge1加在order的末尾   
+        strcat(order, " ");             //命令中间的空格    
+        strcat(order, messge2);         //把messge2加在order的末尾     
+        sprintf(buff, order);           //把调整格式的order存入buff
+
+    //help  
+        if (strncmp(messge1, "help", 4) == 0) {
+            help();
+        }
+
+
+        if (strncmp(messge1, "quit", 4) == 0)
+        {
+            printf("                    欢迎再次进入FAKE_FTP，谢谢使用！\n");
+            closesocket(sockclient);
+            WSACleanup();
+            return 0;
+        }
+        TCPSend(buff);//发送buff里面的数据        
+        recv(sockclient, rbuff, 1024, 0);
+        printf(rbuff);
+
+        if (strncmp(rbuff, "get", 3) == 0)
+        {
+            fd = fopen(messge2, "wb");
+            if (fd == NULL)
+            {
+                printf("open file %s for weite failed!\n", messge2);
+                return 0;
+            }
+            while ((count = recv(sockclient, rbuff, 1024, 0)) > 0)
+            {
+                fwrite(rbuff, sizeof(rbuff), count, fd);
+            }
+
+            fclose(fd);
+        }
+
+        if (strncmp(rbuff, "put", 3) == 0)   //put
+        {
+            strcpy(filename, rbuff + 9);
+            fd2 = fopen(filename, "rb");
+            if (fd2)
+            {
+                if (!SendFile(sockclient, fd2)) {
+                    printf("send failed!");
+                    return 0;
+                }
+                fclose(fd2);
+            }
+
+            else
+            {
+                strcpy(sbuff, "can't open file!\n");
+                if (send(sockclient, sbuff, 1024, 0))
+                    return 0;
+            }
+        }
+
+        if (strncmp(rbuff, "ls", 2) == 0)      //ls
+        {
+            printf("\n");
+            list(sockclient);               //列出接受到的列表内容
+        }
+        if (strncmp(rbuff, "pwd", 3) == 0)
+        {
+            list(sockclient);               //列出接受到的内容--绝对路径
+        }
+        if (strncmp(rbuff, "cd", 2) == 0) {}      //cd
+
+        closesocket(sockclient);            //关闭连接
+        WSACleanup(); 
+    } 
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,244 @@
+﻿#pragma warning(disable:4996)  
+#include "Winsock.h"
+#include "windows.h"
+#include "stdio.h"  
+#define RECV_PORT 4523  
+#define SEND_PORT 4623  
+#pragma   comment(lib, "wsock32.lib")
+#include<string>
+using namespace std;
+
+SOCKET sockclient, sockserver;
+struct sockaddr_in ServerAddr;  //服务器地址
+struct sockaddr_in ClientAddr;  //客户端地址
+int Addrlen;        //地址长度
+char filename[20];  //文件名
+char order[10];     //命令  
+char rbuff[1024];   //接收缓冲区  
+char sbuff[1024];   //发送缓冲区  
+
+
+DWORD StartSock()    //初始化w
+{
+    WSADATA WSAData;
+    if (WSAStartup(MAKEWORD(2, 2), &WSAData) != 0)
+    {
+        printf("socket init fail!\n");
+        return (-1);
+    }
+    return(1);
+}
+
+DWORD CreateSocket()
+{
+    sockclient = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockclient == SOCKET_ERROR)
+    {
+        printf("sockclient create fail ! \n");
+        WSACleanup();
+        return(-1);
+    }
+    ServerAddr.sin_family = AF_INET;
+    ServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    ServerAddr.sin_port = htons(RECV_PORT);
+    if (bind(sockclient, (struct  sockaddr  FAR*) & ServerAddr, sizeof(ServerAddr)) == SOCKET_ERROR) //bind函数将套接字和地址结构绑定   
+    {                     
+        printf("bind is the error");
+        return(-1);
+    }
+    return (1);
+}
+
+int SendFileRecord(SOCKET datatcps, WIN32_FIND_DATA* pfd)     //用来发送当前文件记录
+{
+    char filerecord[MAX_PATH + 32];
+    FILETIME ft;         //文件建立时间   
+    FileTimeToLocalFileTime(&pfd->ftLastWriteTime, &ft);
+    SYSTEMTIME lastwtime;     //SYSTEMTIME系统时间数据结构   
+    FileTimeToSystemTime(&ft, &lastwtime);
+    char* ls = pfd->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ? "<DIR>" : " ";
+    sprintf(filerecord, "%04d-%02d-%02d %02d:%02d  %5s %10d   %-20s\n",
+        lastwtime.wYear,
+        lastwtime.wMonth,
+        lastwtime.wDay,
+        lastwtime.wHour,
+        lastwtime.wMinute,
+        ls,
+        pfd->nFileSizeLow,
+        pfd->cFileName);
+    if (send(datatcps, filerecord, strlen(filerecord), 0) == SOCKET_ERROR)
+    {   
+        printf("Error occurs when sending file list!\n");
+        return 0;
+    }
+    return 1;
+}
+
+int SendFileList(SOCKET datatcps) //遍历可执行文件所在目录，并发送
+{
+    HANDLE hff;
+
+    char szCurPath[MAX_PATH];
+    GetCurrentDirectory(sizeof(szCurPath), szCurPath);
+    WIN32_FIND_DATA fd;   
+ 
+    char end[10] = "\\*.*";
+    strcat(szCurPath, end);
+    hff = FindFirstFile(szCurPath, &fd);
+     
+    if (hff == INVALID_HANDLE_VALUE)//错误处理 
+    {
+        const char* errstr = "can't list files!\n";
+        printf("list file error!\n");
+        if (send(datatcps, errstr, strlen(errstr), 0) == SOCKET_ERROR)
+        {
+            printf("error occurs when senging file list!\n");
+        }
+        closesocket(datatcps);
+        return 0;
+    }
+    SendFileRecord(datatcps, &fd); //发送第一项文件
+   
+    while (FindNextFile(hff, &fd))
+    {  
+        
+        SendFileRecord(datatcps, &fd);//遍历目录下所有文件，并发送
+ 
+    }
+    closesocket(datatcps);
+    return 1;
+}
+
+int SendFile(SOCKET datatcps, FILE* file)
+{
+    printf(" sending file data..");
+    for (;;)   //从文件中循环读取数据并发送客户端   
+    {
+        int r = fread(sbuff, 1, 1024, file);   //把file里面的内容读到sbuff缓冲区   
+        if (send(datatcps, sbuff, r, 0) == SOCKET_ERROR)
+        {
+            printf("lost the connection to client!\n");
+            closesocket(datatcps);
+            return 0;
+        }
+        if (r < 1024) 
+            break;
+    }
+    closesocket(datatcps);
+    printf("done\n");
+    return 1;
+}
+
+DWORD ConnectProcess()
+{
+    Addrlen = sizeof(ClientAddr);
+    if (listen(sockclient, 5) < 0)
+    {
+        printf("Listen error");
+        return(-1);
+    }
+    printf("server is listening...\n");
+    for (;;)
+    {
+        sockserver = accept(sockclient, (struct sockaddr FAR*) & ClientAddr, &Addrlen);
+
+        for (;;)
+        {
+            memset(rbuff, 0, 1024);
+            memset(sbuff, 0, 1024);
+            if (recv(sockserver, rbuff, 1024, 0) <= 0)
+            {
+                break;
+            }
+            printf("\n");
+            printf("获取并执行的命令为：");
+            printf(rbuff);
+            if (strncmp(rbuff, "get", 3) == 0)
+            {
+                strcpy(filename, rbuff + 4); printf(filename);
+                FILE* file; //定义一个文件访问指针    
+                //处理下载文件请求    
+                file = fopen(filename, "rb");//打开下载的文件，只允许读写   
+                if (file)
+                {
+                    sprintf(sbuff, "get file %s\n", filename);
+                    if (!send(sockserver, sbuff, 1024, 0))
+                    {
+                        fclose(file);      return 0;
+                    }
+                    else
+                    {//创建额外数据连接传送数据     
+                        if (!SendFile(sockserver, file))
+                            return 0;
+                        fclose(file);
+                    }
+                }//file   
+
+                else//打开文件失败    
+                {
+                    strcpy(sbuff, "can't open file!\n");
+                    if (send(sockserver, sbuff, 1024, 0))
+                        return 0;
+                } //lost
+            }//get
+
+            if (strncmp(rbuff, "put", 3) == 0)
+            {
+                FILE* fd;
+                int count;
+                strcpy(filename, rbuff + 4);
+                fd = fopen(filename, "wb");
+                if (fd == NULL)
+                {
+                    printf("open file %s for weite failed!\n", filename);
+                    return 0;
+                }
+                sprintf(sbuff, "put file %s", filename);
+                if (!send(sockserver, sbuff, 1024, 0))
+                {
+                    fclose(fd);
+                    return 0;
+                }
+                while ((count = recv(sockserver, rbuff, 1024, 0)) > 0)//recv函数返回接受的字节数赋给count          
+                    fwrite(rbuff, sizeof(char), count, fd);
+               
+                printf(" get %s succed!\n", filename);
+                fclose(fd);
+            }//put
+
+            if (strncmp(rbuff, "pwd", 3) == 0) {
+                char   path[1000];
+                GetCurrentDirectory(1000, path);//找到当前进程的当前目录  
+                strcpy(sbuff, path);
+                send(sockserver, sbuff, 1024, 0);
+            }//pwd   
+
+            if (strncmp(rbuff, "ls", 2) == 0) {
+                strcpy(sbuff, rbuff);
+                send(sockserver, sbuff, 1024, 0);
+                SendFileList(sockserver);//发送当前列表
+            }//ls
+
+            if (strncmp(rbuff, "cd", 2) == 0)
+            {
+                strcpy(filename, rbuff + 3);
+                strcpy(sbuff, rbuff);
+                send(sockserver, sbuff, 1024, 0);
+                SetCurrentDirectory(filename);  //设置当前目录
+            }//cd  
+            closesocket(sockserver);
+        }//for 2
+    }//for 1
+}
+
+
+int main()
+{
+    if (StartSock() == -1)
+        return(-1);
+    if (CreateSocket() == -1)
+        return(-1);
+    if (ConnectProcess() == -1)
+        return(-1);
+    return(1);
+}
